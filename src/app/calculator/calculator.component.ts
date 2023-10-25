1975b4ca219452d142e9848ea8c8786eb009350e
Kent010341 <kent010341@gmail.com>
2023-10-26 00:45:03 +0800

    add event listener

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,22 +1,51 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MatChipInputEvent } from '@angular/material/chips';
+import { Observable, Subject, of, switchMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css']
 })
-export class CalculatorComponent {
+export class CalculatorComponent implements OnDestroy {
 
   chips: CaculateUnit[] = [];
 
   showWarning: boolean = false;
 
+  /** 公式異動事件 */
+  formulaChanged$ = new Subject<void>();
+
+  /** 計算結果字串 */
+  result: string = '';
+
+  /** component destroy */
+  destroy$ = new Subject<void>();
+
   // patterns
   private readonly WORKDAY_PATTERN = /^([0-9]+\.*[0-9]*[wdhm])+$/;
   private readonly OPERATOR_PATTERN = /^[\+\-\*\/]$/;
-  private readonly NUMBER_PATTERN = /[0-9]+\.*[0-9]*/;
+  private readonly NUMBER_PATTERN = /^[0-9]+\.*[0-9]*$/;
+
+  constructor() {
+    this.formulaChanged$.pipe(
+      takeUntil(this.destroy$),
+      switchMap(() => this.calculateResult()),
+    ).subscribe({
+      next: result => {
+        this.result = result;
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   add(event: MatChipInputEvent): void {
     const value = (event.value || '').replace(/\s+/, '');
@@ -35,14 +64,17 @@ export class CalculatorComponent {
 
     this.chips.push(chip);
     event.chipInput.clear();
+    this.formulaChanged$.next();
   }
 
   remove(index: number): void {
     this.chips.splice(index, 1);
+    this.formulaChanged$.next();
   }
 
   drop(event: CdkDragDrop<CaculateUnit[]>) {
     moveItemInArray(this.chips, event.previousIndex, event.currentIndex);
+    this.formulaChanged$.next();
   }
 
   getChipClass(chip: CaculateUnit): string {
@@ -56,6 +88,11 @@ export class CalculatorComponent {
       default:
         return '';
     }
+  }
+
+  private calculateResult(): Observable<string> {
+    // TODO 實作計算邏輯
+    return of(this.chips.map(c => c.rawValue).join(','));
   }
 
 }
